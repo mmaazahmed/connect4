3450ed34dd8efc83f51a5de8173a519e87e2f803
mmaazahmed <maazahmedpoke@gmail.com>
2024-02-10 09:59:04 +0500

    refactored play function

--- play.py
+++ play.py
@@ -4,50 +4,35 @@ from player import Player
 from minmax_agent import MinmaxAgent
 Connect4=Connect4()
 def get_players():
-    players=[]
-    fighter=input("Choose your Fighter: 1:Human, 2:AI\n ")
-    fighter_name=input("Enter figher name\n")
-    opponent=input("Choose your Opponent: 1:Human, 2:AI\n")
-    opponent_name=input("Enter opponent name\n")
+    players = []
+    for i in range(2):
+        player_type = input(f"Choose your Player {i+1}: 1: Human, 2: AI\n ")
+        player_name = input(f"Enter player {i+1} name\n")
 
-    if int(fighter)==1:
-        Player1=Player(fighter_name)
-        players.append(Player1)
-    else:
-        Player1=MinmaxAgent(fighter_name)
-        players.append(Player1)
-    
-    if int(opponent)==1:
-        Player2=Player(opponent_name)
-        players.append(Player2)
-    else:
-        Player2=MinmaxAgent(opponent_name)
-        players.append(Player2)
+        player = Player(player_name) if player_type == '1' else MinmaxAgent(player_name)
+        players.append(player)
+
     return players
 
 def play(Game,isPlayerTurn,players):
     winner=''
     game_state=True
-    round=1
-    # player1=Player("player1")
-    # Bob=MinmaxAgent('Bob')
-    while game_state:
-        if isPlayerTurn:
-            player_move=players[0].get_move(Game)
-            Connect4.drop_piece(player_move,'x')
-            if Connect4.is_winning_move(player_move,'x'):
-                game_state=False
-                winner=players[0].get_name()
-        else:
-            ai_move=players[1].get_move(Game)
-            Connect4.drop_piece(ai_move,'o')
-            Connect4.display_board(round)
-            round+=1
-            if Connect4.is_winning_move(ai_move,'o'):
-                game_state=False
-                winner=players[1].get_name()
+    current_round=1
+    while  True:
+        current_player=players[0] if isPlayerTurn else players[1]
+        piece='x'if isPlayerTurn else 'o'
+        column=current_player.get_move(Game)
+        Connect4.drop_piece(player_move, piece )
 
+        if Connect4.is_winning_move(player_move,piece):
+            winner=current_player.get_name()
+            break
+        
+        if not isPlayerTurn:
+            Connect4.display_board(current_round)
+    
         isPlayerTurn= not (isPlayerTurn)
+        current_round+=1
     Connect4.display_board(round)
     print("game won by",winner)
 
